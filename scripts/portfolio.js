3e793b02c494317722141edea4066410e786a66c
Pablo Martinez <pmartinez@mixpo.com>
2016-02-07 18:48:30 -0800

    finished

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,74 +1,63 @@
-var projects = [];
-
-function Project (opts) {
-  this.author = opts.author;
-  this.title = opts.title;
-  this.category = opts.category;
-  this.authorUrl = opts.authorUrl;
-  this.publishedOn = opts.publishedOn;
-  this.body = opts.body;
-}
-
-Project.prototype.toHtml = function() {
-  var projectTemplate = $('#project-template').html();
-  var compileTemplate = Handlebars.compile(projectTemplate);
-
-  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  this.more = '<a class="more">MORE</a>';
-  this.hr = '<hr>';
-  // this.append('<hr>');
-  var html = compileTemplate(this);
-  $('.projects').append(html);
-
-};
-
-placeHolder.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
-
-placeHolder.forEach(function(ele) {
-  projects.push(new Project(ele));
-});
-
-projects.forEach(function(a){
-  $('.projects').append(a.toHtml());
-});
-
-$(document).ready(function(){
-  $('#home-btn').click(function(){
-    $('.about-me').addClass('hide');
-    $('.projects').removeClass('hide');
+  function Project (opts) {
+    this.author = opts.author;
+    this.title = opts.title;
+    this.category = opts.category;
+    this.authorUrl = opts.authorUrl;
+    this.publishedOn = opts.publishedOn;
+    this.body = opts.body;
   }
-);});
-$(document).ready(function(){
-  $('#about-btn').click(function(){
-    $('.projects').addClass('hide');
-    $('.about-me').removeClass('hide');
-  }
-);});
 
-var articleFunctions = {};
+  Project.all = [];
 
-articleFunctions.showGlimpse = function() {
-  $('.project-body *:nth-of-type(n+2)').hide(); // Hide elements beyond the first 2 in any article body.
-  $('.projects').on('click','.more', function(event){
-    event.preventDefault();
-    $(this).parent().find('*').show();
-    $(this).replaceWith('<a class="less">LESS</a>');
-  });
-};
+  // Project.prototype.toHtml = function() {
+  //   var projectTemplate = $('#project-template').html();
+  //   var compileTemplate = Handlebars.compile(projectTemplate);
+  //
+  //   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  //   this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  //   this.more = '<a class="more">MORE</a>';
+  //   this.hr = '<hr>';
+  //   // this.append('<hr>');
+  //   var html = compileTemplate(this);
+  //   $('.projects').append(html);
+  //
+  // };
 
-articleFunctions.removeGlimpse = function(){
-  $('.projects').on('click','.less', function(event){
-    event.preventDefault();
-    $('.project-body *:nth-of-type(n+2)').hide();
-    $(this).replaceWith('<a class="more">MORE</a>');
-  });
-};
+  Project.prototype.toHtml = function() {
+    var template = Handlebars.compile($('#project-template').text());
 
-articleFunctions.showGlimpse();
-articleFunctions.removeGlimpse();
+    this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+    this.body = this.body;
+    this.more = '<a class="more">MORE</a>';
+    this.hr = '<hr>';
+
+    return template(this);
+  };
+
+  Project.loadAll = function(rawData) {
+    rawData.sort(function(a,b) {
+      return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+    });
+
+    rawData.forEach(function(ele) {
+      Project.all.push(new Project(ele));
+    });
+  };
+
+  Project.fetchAll = function() {
+    if (localStorage.rawData) {
+      Project.loadAll(JSON.parse(localStorage.rawData));
+      articleFunctions.initIndexPage();
+    } else {
+      $.getJSON('data/inform.json', function(data) {
+        console.log(data);
+        Project.loadAll(data);
+        localStorage.rawData = JSON.stringify(data);
+        articleFunctions.initIndexPage();
+      });
+    }
+  };
 
 //had to make a change
 // var test = this.publishedOn;
